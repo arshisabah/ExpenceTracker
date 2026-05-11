e128e129f9adf9546d4c3ae60954c4c27520538e
Arshi <arshisabah326@gmail.com>
2026-05-11 23:05:16 +0530

    Added Expense Tracker project with OOP, Collections, and File Handling

--- src/main/java/com/expensetracker/service/ExpenseService.java
+++ src/main/java/com/expensetracker/service/ExpenseService.java
@@ -1,4 +1,5 @@
 package com.expensetracker.service;
+import java.io.*;
 import com.expensetracker.model.Expense;
 
 import java.util.ArrayList;
@@ -6,9 +7,34 @@ import java.util.ArrayList;
 public class ExpenseService {
 
     private ArrayList<Expense> expenses = new ArrayList<>();
+    //file path
+    private final String FILE_NAME = "data/expenses.txt";
+    //save expences to file
+    private void saveExpensesToFile() {
+
+        try (BufferedWriter writer =
+                     new BufferedWriter(new FileWriter(FILE_NAME))) {
+
+            for (Expense expense : expenses) {
+
+                writer.write(
+                        expense.getId() + "," +
+                                expense.getTitle() + "," +
+                                expense.getAmount() + "," +
+                                expense.getCategory()
+                );
+
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error Saving Expenses");
+        }
+    }
     //add expense
     public void addExpense(Expense expense) {
         expenses.add(expense);
+        saveExpensesToFile();
         System.out.println("Expense added successfully");
     }
     //view all expenses
